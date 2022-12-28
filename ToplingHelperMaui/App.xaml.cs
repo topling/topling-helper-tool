8edfb61b2571aad21b3476a3bf546c70d4e5be37
ZengJingtao <ytztao@topling.cn>
2022-12-28 17:14:48 +0800

    跨平台的窗口大小设定代码(试验)

--- ToplingHelperMaui/App.xaml.cs
+++ ToplingHelperMaui/App.xaml.cs
@@ -5,8 +5,27 @@
         public App()
         {
             InitializeComponent();
-
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+//#if WINDOWS
+            const int WindowWidth = 400;
+            const int WindowHeight = 300;
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+//#endif
+            });
             MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            Window window = base.CreateWindow(activationState);
+            return window;
         }
     }
 }
