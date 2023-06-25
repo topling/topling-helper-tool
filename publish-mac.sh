841b6ae8476626ed9c23638327096a3db2cbb1b3
ZengJingtao <ytztao@topling.cn>
2023-06-25 15:16:31 +0800

    Fix publish-mac.sh

--- publish-mac.sh
+++ publish-mac.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+rm -rf publish
 dotnet publish ToplingHelper.Ava -c release --runtime osx-x64 --self-contained true /p:PublishSingleFile=true  /p:EnableCompressionInSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:IncludeAllContentForSelfExtract=true -o publish
 
-cat << publish/c >> EOF
+cat >> publish/start-toplinghelper.sh << EOF
 #!/bin/bash
 # 运行 bash start-toplinghelper.sh 启动程序
 xattr -rd com.apple.quarantine ToplingHelper
